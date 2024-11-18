56d8ba6b466040e135f3606aa5b5901bc184d346
intellectus-rojiwon <jiwon.ro@int2.us>
2024-11-18 17:41:39 +0900

    add health check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,10 @@ import * as nest from "@nestjs/common";
 import { UserModule } from "@/app/user/user.module";
 import { InfraModule } from "@/infrastructure/infra.module";
 
+import { HealthController } from "./system/health.controller";
+
 @nest.Module({
     imports: [InfraModule, UserModule],
+    controllers: [HealthController],
 })
 export class AppModule {}
